7f5fad81d7c5c97401a89c10f83bcb24e8d7d0f3
Saransh Varshney <varshneysaransh6045@gmail.com>
2026-04-05 17:19:40 +0530

    Defined the problem class

--- backend/src/Main.java
+++ backend/src/Main.java
@@ -1,3 +1,15 @@
+import model.problem;
+
 public class Main {
-    
+    public static void main(String[] args) {
+
+        problem p1 = new problem(
+                "Two Sum",
+                "Arrays",
+                "Easy",
+                "https://leetcode.com/problems/two-sum"
+        );
+
+        System.out.println(p1);
+    }
 }
